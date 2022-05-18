746a4767de658e40b1b218aad8a735036e8d2a3c
VITALII NAGIBIN <nagibin87@yandex.ru>
2022-05-18 19:02:11 +0300

    dz05
    dz05

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace vn_mode_csharp_dz05
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string firstName = "Нагибин";
+            string lastName = "Виталий";
+            string bufferString;
+
+            Console.WriteLine($"Ваше имя: {firstName}, ваша фамилия: {lastName}.");
+            Console.WriteLine("Ой, кто-то перепутал местами имя и фамилию. Чтобы исправить, введите слово ИСПРАВИТЬ и нажмите ENTER.");
+            Console.ReadLine();
+
+            bufferString = lastName;
+            lastName = firstName;
+            firstName = bufferString;
+
+            Console.WriteLine($"Ваше имя: {firstName}, ваша фамилия: {lastName}.");
+            Console.WriteLine("Вот теперь всё правильно!");
+        }
+    }
+}
+
+
+
+
